06c0299e7b420b2003bc6b51df1d41409445f593
leojinjin_IT <leojinjin_IT@126.com>
2019-09-21 20:27:11 +0800

    谷粒商城初始化创建

--- gmall-wms/src/main/java/com/atguigu/gmall/wms/GmallWmsApplication.java
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/GmallWmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GmallWmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GmallWmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GmallWmsApplication.class, args);
+    }
 
 }
